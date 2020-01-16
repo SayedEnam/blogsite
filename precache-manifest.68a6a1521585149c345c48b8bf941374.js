8203ee2835eb4c4e77d279e4c78946cc4dd2405c
SayedEnam <syedenamshohag@gmail.com>
2020-01-16 23:18:01 +0600

    Updates

---
+++ precache-manifest.68a6a1521585149c345c48b8bf941374.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a377d51501ff2b03efd19219c8ab8036",
+    "url": "/blog-app/index.html"
+  },
+  {
+    "revision": "12d687abd43e16ab4282",
+    "url": "/blog-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4ff007f4cc2517eb113a",
+    "url": "/blog-app/static/js/2.6d11143a.chunk.js"
+  },
+  {
+    "revision": "12d687abd43e16ab4282",
+    "url": "/blog-app/static/js/main.25bf52de.chunk.js"
+  },
+  {
+    "revision": "22b0443c2086fbe7a9a6",
+    "url": "/blog-app/static/js/runtime-main.a04d40e2.js"
+  }
+]);
